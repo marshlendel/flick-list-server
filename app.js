92f8a9feb10914e1fb3cad35faca69aa507dffe4
Marshall Brown <marshallendel@gmail.com>
2021-07-28 15:57:04 -0400

    Add db connection

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const Express = require("express")
 const app = Express()
 
-app.listen(4000, ()=> {
-    console.log("[Server] is listening on port 4000")
-})
+//! Imports
+const dbConnection = require("./db")
+
+dbConnection.authenticate()
+    .then(() => dbConnection.sync())
+    .then(() => {
+        app.listen(4000, ()=> {
+            console.log("[Server] is listening on port 4000")
+        })
+    })
+    .catch((err) => console.log(err))
+
