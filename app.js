164b31334bc33b3290994f42005ea855fbbd943c
Anne Porter <a.porter9212@gmail.com>
2021-07-28 16:03:31 -0400

    first commit

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const Express = require("express")
+const db = require("./db")
 const app = Express()
 
-app.listen(4000, ()=> {
-    console.log("[Server] is listening on port 4000")
+//!!Imports
+const dbConnection =  require("./db")
+
+dbConnection.authenticate()
+.then(db => dbConnection.sync())
+.then(() => {
+    app.listen(4000, ()=> {
+        console.log("[Server]: App is listening on port 4000");
+    });
 })
+.catch((err) => {
+    console.log(`[Server]: Server crashed. Error = ${err}`); 
+});
+
---
+++ db.js
@@ -0,0 +1,5 @@
+const Sequelize = require("sequelize");
+
+const dbConnection = new Sequelize("postgres://postgres:pgsquirrel@localhost:5432/flick-list")
+
+module.exports = dbConnection;
