9e7362b8bf66c2a807bd937dba2d5819e7c036c9
Anne Porter <a.porter9212@gmail.com>
2021-07-29 14:28:35 -0400

    added .env file

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const Sequelize = require("sequelize")
-const dbConnection = new Sequelize("postgres://postgres:pgsquirrel@localhost:5432/flick-list")
+const dbConnection = new Sequelize("postgres://postgres:db_Password@localhost:5432/flick-list")
 
 module.exports = dbConnection
 
